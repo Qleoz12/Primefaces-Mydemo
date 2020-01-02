fb98083ef77beeeb09106ddd9d1743b8d3ba713c
Dann <qleoz12.leonardo@gmail.com>
2020-01-02 12:00:39 -0500

    cambios realizados fix menu fix picklist  tes composite component

--- src/main/java/Beans/PickListBean.java
+++ src/main/java/Beans/PickListBean.java
@@ -2,6 +2,7 @@ package Beans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -9,20 +10,19 @@ import javax.inject.Named;
 
 import org.primefaces.model.DualListModel;
 
+import Models.Employee;
+
 @Named
 @javax.faces.view.ViewScoped
-public class CompositeComponent implements Serializable {
+public class PickListBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1391438493899480075L;
+	private DualListModel<Employee> listModel;
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CompositeComponent.class);
 	private DualListModel<String> cities;
 	List<String> citiesSource = new ArrayList<String>();
 	List<String> citiesTarget = new ArrayList<String>();
-
-	public CompositeComponent() {
-		super();
-	}
-
+	
 	@PostConstruct
 	public void init() {
 		// Cities
@@ -35,7 +35,26 @@ public class CompositeComponent implements Serializable {
 		citiesSource.add("Rome");
 
 		cities = new DualListModel<String>(citiesSource, citiesTarget);
+		// initial unselected list
+		List<Employee> sourceList = Arrays.asList(new Employee("1", "Jim", "IT"), new Employee("2", "Sara", "Sale"),
+				new Employee("3", "Tom", "Admin"), new Employee("4", "Diana", "IT"));
+
+		// initial selected list
+		List<Employee> destinationList = new ArrayList<>();
+		destinationList.add(new Employee("5", "Jessica", "Sale"));
+
+		listModel = new DualListModel<>(new ArrayList<>(sourceList), destinationList);
 	}
+
+	public DualListModel<Employee> getListModel() {
+		return listModel;
+	}
+
+	public void setListModel(DualListModel<Employee> listModel) {
+		this.listModel = listModel;
+	}
+
+
 
 	public DualListModel<String> getCities() {
 		return cities;
@@ -44,5 +63,4 @@ public class CompositeComponent implements Serializable {
 	public void setCities(DualListModel<String> cities) {
 		this.cities = cities;
 	}
-
 }
---
+++ src/main/java/converter/PickListEmpConverter.java
@@ -0,0 +1,37 @@
+package converter;
+
+import org.primefaces.component.picklist.PickList;
+import org.primefaces.model.DualListModel;
+
+import Models.Employee;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+@FacesConverter(value = "empConverter")
+public class PickListEmpConverter implements Converter {
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent comp, String value) {
+       
+		DualListModel<Employee> model = (DualListModel<Employee>) ((PickList) comp).getValue();
+        for (Employee employee : model.getSource()) {
+            if (employee.getId().equals(value)) {
+                return employee;
+            }
+        }
+        for (Employee employee : model.getTarget()) {
+            if (employee.getId().equals(value)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent comp, Object value) {
+        return ((Employee) value).getId();
+    }
+}
